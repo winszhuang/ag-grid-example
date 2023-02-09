bcdd56e96cf47ee902212a6513ed0205e1556b2e
wins <win4709hi@gmail.com>
2023-02-09 11:27:21 +0800

    feat: base ag-grid feature

--- app.vue
+++ app.vue
@@ -1,5 +1,52 @@
+<script setup lang="ts">
+import { AgGridVue } from 'ag-grid-vue3'
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+interface RowData {
+  make: string,
+  model: string,
+  price: number
+}
+
+interface ColumnDef {
+  field: string,
+  sortable: boolean,
+  filter: boolean
+}
+
+const columnDefs = ref<ColumnDef[]>([
+  { field: 'make', sortable: true, filter: true },
+  { field: 'model', sortable: true, filter: true },
+  { field: 'price', sortable: true, filter: true },
+])
+
+const rowData = ref<RowData[]>([
+  { make: 'Vauxhall', model: 'Corsa', price: 17300 },
+  { make: 'Ford', model: 'Fiesta', price: 18000 },
+  { make: 'Volkswagen', model: 'Golf', price: 26000 },
+])
+
+const { data, error } = useFetch('https://www.ag-grid.com/example-assets/row-data.json')
+
+watch(data, (value) => {
+  rowData.value = value as unknown as RowData[]
+})
+
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div v-if="!error">
+    <ag-grid-vue 
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      class="ag-theme-alpine"
+      style="height: 500px"
+      rowSelection="multiple"
+      animateRows="true"
+    />
+  </div>
+  <div v-else>
+    fetch data get some error!!
   </div>
 </template>
